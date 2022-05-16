d7890304fd3ddde5618caa931106fab6f7309061
Jepoy Factoran <factoran20@gmail.com>
2022-05-16 21:29:44 +0800

    added some random user data to test

---
+++ app.js
@@ -0,0 +1,26 @@
+const data = [
+  {
+    name: "John Doe",
+    age: 32,
+    gender: "male",
+    lookingfor: "female",
+    location: "Sta. Rosa Laguna",
+    image: `https://randomuser.me/api/portraits/men/${this.age}.jpg`,
+  },
+  {
+    name: "Jen Smith",
+    age: 24,
+    gender: "female",
+    lookingfor: "male",
+    location: "Sta. Rosa Laguna",
+    image: `https://randomuser.me/api/portraits/women/${this.age}.jpg`,
+  },
+  {
+    name: "Balanar Enes",
+    age: 27,
+    gender: "male",
+    lookingfor: "female",
+    location: "Sta. Rosa Laguna",
+    image: `https://randomuser.me/api/portraits/men/${this.age}.jpg`,
+  },
+];
